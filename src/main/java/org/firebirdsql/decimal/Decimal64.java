1b3774e844b15887bd504fa6c490b344776089d7
Mark Rotteveel <mark@lawinegevaar.nl>
2023-06-30 13:03:13 +0200

    Update copyright

--- src/main/java/org/firebirdsql/decimal/Decimal64.java
+++ src/main/java/org/firebirdsql/decimal/Decimal64.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Firebird development team and individual contributors
+ * Copyright (c) 2018-2023 Firebird development team and individual contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
