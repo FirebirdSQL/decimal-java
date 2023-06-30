a95a7f0d615f64004ca6e208942513827aee4c72
mrotteveel <mrotteveel@users.noreply.github.com>
2023-06-30 10:56:52 +0000

    Deploying to gh-pages from @ FirebirdSQL/decimal-java@aac9c179030c9aa85557711d677785d163b9635d 🚀

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.firebirdsql.decimal"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"org.firebirdsql.decimal","l":"org.firebirdsql.decimal"}];updateSearchResults();
